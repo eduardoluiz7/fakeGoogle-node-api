d51c48d94aa16a52ddbb68b92fb5d550232b6e62
Eduardo Luiz <els6@cin.ufpe.br>
2019-08-01 14:13:04 -0300

    Rota para pegar parâmetros via url

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
     
-app.use('/', (req, res)=>{
+app.use('/get', (req, res)=>{
     if(!req.body.key || !(req.body.key == consts.key)){
         res.status(500).send({message: 'Invalid key!'});
         }else{
@@ -43,5 +43,45 @@ app.use('/', (req, res)=>{
         res.status(200).send(data);
     }
     });
+	
+app.use('/', (req, res)=>{
+	let key = req.query.key;
+	let origin = [parseInt(req.query.origin[0]),parseInt(req.query.origin[1])]
+	let destination = [parseInt(req.query.destination[0]),parseInt(req.query.destination[1])]
+    if(!key || !(key == consts.key)){
+        res.status(500).send({message: 'Invalid key!'});
+        }else{
+        console.log("Authenticated key!");
+        let distance = operations.distance(origin, destination);
+        let duration = distance/50;
+
+        let data = {
+                destination_addresses : [ destination ],
+                origin_addresses : [ origin ],
+                rows : [
+                {
+                    elements : [
+                        {
+                            distance : {
+                            text : "distancia em texto:",
+                            value : distance
+                            },
+                            duration : {
+                            text : "duração em texto",
+                            value : duration
+                            },
+                            status : "OK"
+                        }
+                    ]
+                }
+                ],
+                status : "OK"
+            }
+
+        res.status(200).send(data);
+    }
+    });
+	
+
 
 module.exports = app;
